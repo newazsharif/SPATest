12a032e4c66c8f189889448071e3ed9a91ae45bd
newazsharif <newaz.sharif@hotmail.com>
2015-08-01 17:21:09 +0600

    First commit

---
+++ MvcApplication1/Angular/Route.js
@@ -0,0 +1,24 @@
+﻿var myModule = angular.module('MyApp', ['ngRoute']);
+myModule.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/Index', {
+            templateUrl: 'Home',
+            
+        })
+
+        // route for the about page
+        .when('/about', {
+            templateUrl: 'Home/About',
+            
+        })
+
+        // route for the contact page
+        .when('/contact', {
+            templateUrl: 'Home/Contact',
+            
+        });
+    $locationProvider.html5Mode(true).hashPrefix('!')
+        
+});
